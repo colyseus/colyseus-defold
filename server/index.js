4c7e187a4d31d406b84a303cd1eaba2590998805
Endel Dreyer <endel.dreyer@gmail.com>
2018-12-13 18:00:54 -0200

    use port 2657. update server dependencies.

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const colyseus = require("colyseus");
 const ChatRoom = require('./chat_room');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 2657;
 
 process.on('unhandledRejection', r => console.log(r));
 
