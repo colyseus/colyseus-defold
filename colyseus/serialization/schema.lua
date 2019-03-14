cc79f02ecca1212ae342cbf2ebac58cd913dfab1
Endel Dreyer <endel.dreyer@gmail.com>
2019-03-14 12:27:48 -0300

    implement schema serializer

---
+++ colyseus/serialization/schema.lua
@@ -0,0 +1,32 @@
+local utils = require('colyseus.utils')
+local Schema = require('colyseus.serialization.schema.schema')
+
+local schema_serializer = {}
+schema_serializer.__index = schema_serializer
+
+function schema_serializer.new ()
+  local instance = {
+    state = nil
+  }
+  setmetatable(instance, schema_serializer)
+  return instance
+end
+
+function schema_serializer:get_state()
+  return self.state
+end
+
+function schema_serializer:set_state(encoded_state)
+  self.state:decode(encoded_state)
+end
+
+function schema_serializer:patch(binary_patch)
+  self.state:decode(binary_patch)
+end
+
+function schema_serializer:handshake(bytes)
+  self.state = Schema.reflection_decode(bytes)
+end
+
+return schema_serializer
+
