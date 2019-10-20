8533813bb200682aea8d18d9f49bb54b67982ffd
Endel Dreyer <endel.dreyer@gmail.com>
2019-10-20 18:55:50 -0300

    move tests to use deftest

--- test/delta_container.lua
+++ test/delta_container.lua
@@ -20,237 +20,239 @@ function error_callback(change)
   error("shouldn't enter here")
 end
 
-describe("colyseus", function()
-  local data
-  local container
-  local num_calls
+return function()
+  describe("colyseus", function()
+    local data
+    local container
+    local num_calls
 
-  describe("delta_listener", function()
+    describe("delta_listener", function()
 
-    before_each(function()
-      num_calls = 0
-      data = {
-          players = {
-              one = 1,
-              two = 1
-          },
-          entity = {
-              x = 0, y = 0, z = 0,
-              xp = 100,
-              rotation = 10
-          },
-          entities = {
-              one = { x = 10, y = 0 },
-              two = { x = 0, y = 0 },
-          },
-          chests = {
-              one = { items = { one = 1, } },
-              two = { items = { two = 1, } }
-          },
-          countdown = 10,
-          sequence = { 0, 1, 2, 3, 4, 5 },
-          board = {
-              { 0, 1, 0, 4, 0 },
-              { 6, 0, 3, 0, 0 },
-          },
-      }
+      before(function()
+        num_calls = 0
+        data = {
+            players = {
+                one = 1,
+                two = 1
+            },
+            entity = {
+                x = 0, y = 0, z = 0,
+                xp = 100,
+                rotation = 10
+            },
+            entities = {
+                one = { x = 10, y = 0 },
+                two = { x = 0, y = 0 },
+            },
+            chests = {
+                one = { items = { one = 1, } },
+                two = { items = { two = 1, } }
+            },
+            countdown = 10,
+            sequence = { 0, 1, 2, 3, 4, 5 },
+            board = {
+                { 0, 1, 0, 4, 0 },
+                { 6, 0, 3, 0, 0 },
+            },
+        }
 
-      container = DeltaContainer.new(clone(data))
+        container = DeltaContainer.new(clone(data))
+      end)
+
+      it("should trigger callbacks for initial state", function()
+          local container = DeltaContainer.new({})
+          local callback = function (change)
+            num_calls = num_calls + 1
+          end
+
+          container:listen("players", callback)
+          container:listen("entity", callback)
+          container:listen("entities/:id", callback)
+          container:listen("chests/:id", callback)
+          container:listen("chests/:id/items/:id", callback)
+          container:listen("sequence/:number", callback)
+          container:listen("board/:number/:number", callback)
+
+          container:set(clone(data))
+          assert_same(num_calls, 24)
+      end)
+
+      it("should listen to 'add' operation", function()
+          container:listen("players", error_callback)
+          container:listen("players/:string/:string", error_callback)
+          container:listen("players/:string", function (change)
+              assert_same(change.operation, "add")
+              assert_same(change.raw_path, {"players", "three"})
+              assert_same(change.path.string, "three")
+              assert_same(change.value, 3)
+          end)
+
+          data.players.three = 3;
+          container:set(data)
+      end)
+
+      it("should match the full path", function()
+          container:listen(":name/x", function(change)
+              assert_same(change.path.name, "entity")
+              assert_same(change.value, 50)
+          end);
+
+          container:listen(":name/xp", function (change)
+              assert_same(change.raw_path, {"entity", "xp"})
+              assert_same(change.path.name, "entity")
+              assert_same(change.value, 200)
+          end);
+
+          data.entity.x = 50
+          data.entity.xp = 200
+
+          container:set(data)
+      end)
+
+      it("should listen to 'remove' operation", function()
+          container:listen("players/:name", function (change)
+              assert_same(change.raw_path, { "players", "two" })
+              assert_same(change.path.name, "two");
+              assert_same(change.value, nil);
+          end)
+
+          data.players.two = nil
+          container:set(data)
+      end)
+
+      it("should allow multiple callbacks for the same operation", function()
+          local i = 0
+          local accept = function(change) i = i + 1 end
+
+          container:listen("players/:string/:string", error_callback)
+          container:listen("players/:string", accept)
+          container:listen("players/:string", accept)
+          container:listen("players/:string", accept)
+
+          data.players.three = 3
+          container:set(data)
+
+          assert_same(i, 3)
+      end)
+
+      it("should fill multiple variables on listen", function()
+          container:listen("entities/:id/:attribute", function(change)
+              if (change.path.id == "one") then
+                  assert_same(change.path.attribute, "x")
+                  assert_same(change.value, 20)
+
+              elseif (change.path.id == "two") then
+                  assert_same(change.path.attribute, "y")
+                  assert_same(change.value, 40)
+              end
+          end)
+
+          data.entities.one.x = 20
+          data.entities.two.y = 40
+
+          container:set(data)
+      end)
+
+      it("should create custom placeholder ", function()
+          container:register_placeholder(":xyz", "([xyz])")
+
+          container:listen("entity/:xyz", function(change)
+              if (change.path.xyz == "x") then assert_same(change.value, 1)
+              elseif (change.path.xyz == "y") then assert_same(change.value, 2)
+              elseif (change.path.xyz == "z") then assert_same(change.value, 3)
+              else error_callback() end
+          end)
+
+          data.entity.x = 1
+          data.entity.y = 2
+          data.entity.z = 3
+          data.entity.rotation = 90
+
+          container:set(data)
+      end)
+
+      it("should remove specific listener", function()
+          container:listen("players/ten", function(change)
+            assert_same(change.value.ten, 10)
+          end)
+
+          local listener = container:listen("players/ten", error_callback)
+          container:remove_listener(listener)
+
+          data.players.ten = { ten = 10 }
+          container:set(data)
+      end)
+
+      it("using the same placeholder multiple times in the path", function()
+          container:listen("chests/:id/items/:id", function(change)
+              --
+              -- TODO: the current implementation only populates the last ":id" into `change.path.id`
+              --
+              assert_same(change.path.id, "two")
+              assert_same(change.value, 2)
+          end)
+
+          data.chests.one.items.two = 2
+          container:set(data)
+      end)
+
+      it("should remove all listeners", function()
+          container:listen("players", error_callback)
+          container:listen("players", error_callback)
+          container:listen("entity/:attribute", error_callback)
+          container:remove_all_listeners()
+
+          data.players['one'] = nil
+          data.entity.x = 100
+          data.players.ten = { ten = 10 }
+
+          container:set(data)
+      end)
+
+      it("should trigger default listener as fallback", function()
+          local numCallbacksExpected = 3
+          local numCallbacks = 0
+
+          container:listen("players/:string", function (change)
+              if (change.operation == "add") then
+                  assert_same(change.path.string, "three")
+                  assert_same(change.value, 3)
+
+              elseif (change.operation == "remove") then
+                  assert_same(change.path.string, "two");
+                  assert_same(change.value, nil);
+              end
+              numCallbacks = numCallbacks + 1
+          end)
+
+          container:listen(function (change)
+              assert_same(change.path, { "entity", "rotation" })
+              assert_same(change.operation, "replace")
+              assert_same(change.value, 90)
+              numCallbacks = numCallbacks + 1
+          end)
+
+          data.players.three = 3
+          data.players.two = nil
+          data.entity.rotation = 90
+
+          container:set(data)
+          assert_same(numCallbacks, numCallbacksExpected)
+      end)
+
+      it("should allow removing listeners inside a listener", function()
+          -- local container = DeltaContainer.new({})
+          -- local listener_to_remove = container:listen("entities/:id", function(change)
+          --     container:remove_listener(listener_to_remove)
+          --     num_calls = num_calls + 1
+          -- end)
+          --
+          -- container:listen("players", function(change) num_calls = num_calls + 1 end)
+          -- container:set(clone(data))
+          --
+          -- assert.are.same(num_calls, 2)
+      end)
+
     end)
-
-    it("should trigger callbacks for initial state", function()
-        local container = DeltaContainer.new({})
-        local callback = function (change)
-          num_calls = num_calls + 1
-        end
-
-        container:listen("players", callback)
-        container:listen("entity", callback)
-        container:listen("entities/:id", callback)
-        container:listen("chests/:id", callback)
-        container:listen("chests/:id/items/:id", callback)
-        container:listen("sequence/:number", callback)
-        container:listen("board/:number/:number", callback)
-
-        container:set(clone(data))
-        assert.are.same(num_calls, 24)
-    end)
-
-    it("should listen to 'add' operation", function()
-        container:listen("players", error_callback)
-        container:listen("players/:string/:string", error_callback)
-        container:listen("players/:string", function (change)
-            assert.are.same(change.operation, "add")
-            assert.are.same(change.raw_path, {"players", "three"})
-            assert.are.same(change.path.string, "three")
-            assert.are.same(change.value, 3)
-        end)
-
-        data.players.three = 3;
-        container:set(data)
-    end)
-
-    it("should match the full path", function()
-        container:listen(":name/x", function(change)
-            assert.are.same(change.path.name, "entity")
-            assert.are.same(change.value, 50)
-        end);
-
-        container:listen(":name/xp", function (change)
-            assert.are.same(change.raw_path, {"entity", "xp"})
-            assert.are.same(change.path.name, "entity")
-            assert.are.same(change.value, 200)
-        end);
-
-        data.entity.x = 50
-        data.entity.xp = 200
-
-        container:set(data)
-    end)
-
-    it("should listen to 'remove' operation", function()
-        container:listen("players/:name", function (change)
-            assert.are.same(change.raw_path, { "players", "two" })
-            assert.are.same(change.path.name, "two");
-            assert.are.same(change.value, nil);
-        end)
-
-        data.players.two = nil
-        container:set(data)
-    end)
-
-    it("should allow multiple callbacks for the same operation", function()
-        local i = 0
-        local accept = function(change) i = i + 1 end
-
-        container:listen("players/:string/:string", error_callback)
-        container:listen("players/:string", accept)
-        container:listen("players/:string", accept)
-        container:listen("players/:string", accept)
-
-        data.players.three = 3
-        container:set(data)
-
-        assert.are.same(i, 3)
-    end)
-
-    it("should fill multiple variables on listen", function()
-        container:listen("entities/:id/:attribute", function(change)
-            if (change.path.id == "one") then
-                assert.are.same(change.path.attribute, "x")
-                assert.are.same(change.value, 20)
-
-            elseif (change.path.id == "two") then
-                assert.are.same(change.path.attribute, "y")
-                assert.are.same(change.value, 40)
-            end
-        end)
-
-        data.entities.one.x = 20
-        data.entities.two.y = 40
-
-        container:set(data)
-    end)
-
-    it("should create custom placeholder ", function()
-        container:register_placeholder(":xyz", "([xyz])")
-
-        container:listen("entity/:xyz", function(change)
-            if (change.path.xyz == "x") then assert.are.same(change.value, 1)
-            elseif (change.path.xyz == "y") then assert.are.same(change.value, 2)
-            elseif (change.path.xyz == "z") then assert.are.same(change.value, 3)
-            else error_callback() end
-        end)
-
-        data.entity.x = 1
-        data.entity.y = 2
-        data.entity.z = 3
-        data.entity.rotation = 90
-
-        container:set(data)
-    end)
-
-    it("should remove specific listener", function()
-        container:listen("players/ten", function(change)
-          assert.are.same(change.value.ten, 10)
-        end)
-
-        local listener = container:listen("players/ten", error_callback)
-        container:remove_listener(listener)
-
-        data.players.ten = { ten = 10 }
-        container:set(data)
-    end)
-
-    it("using the same placeholder multiple times in the path", function()
-        container:listen("chests/:id/items/:id", function(change)
-            --
-            -- TODO: the current implementation only populates the last ":id" into `change.path.id`
-            --
-            assert.are.same(change.path.id, "two")
-            assert.are.same(change.value, 2)
-        end)
-
-        data.chests.one.items.two = 2
-        container:set(data)
-    end)
-
-    it("should remove all listeners", function()
-        container:listen("players", error_callback)
-        container:listen("players", error_callback)
-        container:listen("entity/:attribute", error_callback)
-        container:remove_all_listeners()
-
-        data.players['one'] = nil
-        data.entity.x = 100
-        data.players.ten = { ten = 10 }
-
-        container:set(data)
-    end)
-
-    it("should trigger default listener as fallback", function()
-        local numCallbacksExpected = 3
-        local numCallbacks = 0
-
-        container:listen("players/:string", function (change)
-            if (change.operation == "add") then
-                assert.are.same(change.path.string, "three")
-                assert.are.same(change.value, 3)
-
-            elseif (change.operation == "remove") then
-                assert.are.same(change.path.string, "two");
-                assert.are.same(change.value, nil);
-            end
-            numCallbacks = numCallbacks + 1
-        end)
-
-        container:listen(function (change)
-            assert.are.same(change.path, { "entity", "rotation" })
-            assert.are.same(change.operation, "replace")
-            assert.are.same(change.value, 90)
-            numCallbacks = numCallbacks + 1
-        end)
-
-        data.players.three = 3
-        data.players.two = nil
-        data.entity.rotation = 90
-
-        container:set(data)
-        assert.are.same(numCallbacks, numCallbacksExpected)
-    end)
-
-    it("should allow removing listeners inside a listener", function()
-        -- local container = DeltaContainer.new({})
-        -- local listener_to_remove = container:listen("entities/:id", function(change)
-        --     container:remove_listener(listener_to_remove)
-        --     num_calls = num_calls + 1
-        -- end)
-        --
-        -- container:listen("players", function(change) num_calls = num_calls + 1 end)
-        -- container:set(clone(data))
-        --
-        -- assert.are.same(num_calls, 2)
-    end)
-
   end)
-end)
+end
--- test/fossil_delta.lua
+++ test/fossil_delta.lua
@@ -1,81 +1,82 @@
 local fossil_delta = require 'colyseus.serialization.fossil_delta.fossil_delta'
 
-function read_file(file)
-    local f = assert(io.open(file, "rb"))
-    local content = f:read("*all")
-    f:close()
-    return content
-end
+return function()
+  function read_file(file)
+      local f = assert(io.open(file, "rb"))
+      local content = f:read("*all")
+      f:close()
+      return content
+  end
 
-function table.clone(orig)
-    local orig_type = type(orig)
-    local copy
-    if orig_type == 'table' then
-        copy = {}
-        for orig_key, orig_value in pairs(orig) do
-            copy[orig_key] = orig_value
-        end
-    else -- number, string, boolean, etc
-        copy = orig
+  function table.clone(orig)
+      local orig_type = type(orig)
+      local copy
+      if orig_type == 'table' then
+          copy = {}
+          for orig_key, orig_value in pairs(orig) do
+              copy[orig_key] = orig_value
+          end
+      else -- number, string, boolean, etc
+          copy = orig
+      end
+      return copy
+  end
+
+  local function make_array_from_str(str)
+    local arr = {}
+    local i = 0
+
+    while i < string.len(str) do
+      arr[i + 1] = string.byte(str, i + 1)
+      i = i + 1
     end
-    return copy
-end
 
-local function make_array_from_str(str)
-  local arr = {}
-  local i = 0
+    return arr
+  end
 
-  while i < string.len(str) do
-    arr[i + 1] = string.byte(str, i + 1)
+  local origin = {}
+  local target = {}
+  local delta = {}
+
+  local i = 1
+  while i <= 5 do
+    origin[i] = make_array_from_str( read_file("test/data/" .. i .. "/origin") )
+    target[i] = make_array_from_str( read_file("test/data/" .. i .. "/target") )
+    delta[i] = make_array_from_str( read_file("test/data/" .. i .. "/delta") )
     i = i + 1
   end
 
-  return arr
-end
-
-local origin = {}
-local target = {}
-local delta = {}
-
-local i = 1
-while i <= 5 do
-  origin[i] = make_array_from_str( read_file("test/data/" .. i .. "/origin") )
-  target[i] = make_array_from_str( read_file("test/data/" .. i .. "/target") )
-  delta[i] = make_array_from_str( read_file("test/data/" .. i .. "/delta") )
-  i = i + 1
-end
-
-local function to_string(arr)
-  local str = ""
-  for i,v in ipairs(arr) do
-    str = str .. string.char(v)
+  local function to_string(arr)
+    local str = ""
+    for i,v in ipairs(arr) do
+      str = str .. string.char(v)
+    end
+    return str
   end
-  return str
-end
 
-describe("colyseus", function()
-  describe("fossil_delta", function()
-    it("should expose 'create' and 'apply' methods", function()
-      assert.truthy(fossil_delta.create)
-      assert.truthy(fossil_delta.apply)
+  describe("colyseus", function()
+    describe("fossil_delta", function()
+      it("should expose 'create' and 'apply' methods", function()
+        assert(fossil_delta.create)
+        assert(fossil_delta.apply)
+      end)
+
+      it("should create delta", function()
+        assert_same(fossil_delta.create(origin[1], target[1]), delta[1])
+        assert_same(fossil_delta.create(origin[2], target[2]), delta[2])
+        assert_same(fossil_delta.create(origin[3], target[3]), delta[3])
+        assert_same(fossil_delta.create(origin[4], target[4]), delta[4])
+        assert_same(fossil_delta.create(origin[5], target[5]), delta[5])
+      end)
+
+      it("should apply delta", function()
+        assert_same(fossil_delta.apply(table.clone(origin[1]), delta[1]), target[1])
+        assert_same(fossil_delta.apply(table.clone(origin[2]), delta[2]), target[2])
+        assert_same(fossil_delta.apply(table.clone(origin[3]), delta[3]), target[3])
+        assert_same(fossil_delta.apply(table.clone(origin[4]), delta[4]), target[4])
+        assert_same(fossil_delta.apply(table.clone(origin[5]), delta[5]), target[5])
+      end)
+
     end)
-
-    it("should create delta", function()
-      assert.are.same(fossil_delta.create(origin[1], target[1]), delta[1])
-      assert.are.same(fossil_delta.create(origin[2], target[2]), delta[2])
-      assert.are.same(fossil_delta.create(origin[3], target[3]), delta[3])
-      assert.are.same(fossil_delta.create(origin[4], target[4]), delta[4])
-      assert.are.same(fossil_delta.create(origin[5], target[5]), delta[5])
-    end)
-
-    it("should apply delta", function()
-      assert.are.same(fossil_delta.apply(table.clone(origin[1]), delta[1]), target[1])
-      assert.are.same(fossil_delta.apply(table.clone(origin[2]), delta[2]), target[2])
-      assert.are.same(fossil_delta.apply(table.clone(origin[3]), delta[3]), target[3])
-      assert.are.same(fossil_delta.apply(table.clone(origin[4]), delta[4]), target[4])
-      assert.are.same(fossil_delta.apply(table.clone(origin[5]), delta[5]), target[5])
-    end)
-
   end)
-end)
-
+end
