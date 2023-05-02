72dc8bcca4e90bce0d3f63e94d911edb1b530e8c
Endel Dreyer <endel.dreyer@gmail.com>
2023-05-02 14:37:37 -0300

    update server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,31 +1,15 @@
-import http from "http";
-import express from "express";
-import cors from "cors";
+/**
+ * IMPORTANT:
+ * ---------
+ * Do not manually edit this file if you'd like to use Colyseus Arena
+ *
+ * If you're self-hosting (without Arena), you can manually instantiate a
+ * Colyseus Server as documented here: 👉 https://docs.colyseus.io/server/api/#constructor-options
+ */
+import { listen } from "@colyseus/tools";
 
-import { Server } from "colyseus";
-import { monitor } from "@colyseus/monitor";
-import { WebSocketTransport } from "@colyseus/ws-transport";
+// Import arena config
+import app from "./app.config";
 
-import { MyRoom } from "./rooms/MyRoom";
-
-const port = Number(process.env.PORT || 2567);
-const app = express()
-
-app.use(cors());
-app.use(express.json())
-
-const server = http.createServer(app);
-const gameServer = new Server({
-  transport: new WebSocketTransport({
-    server,
-  })
-});
-
-// register your room handlers
-gameServer.define('my_room', MyRoom);
-
-// register colyseus monitor AFTER registering your room handlers
-app.use("/colyseus", monitor());
-
-gameServer.listen(port);
-console.log(`Listening on ws://localhost:${ port }`)
+// Create and listen on 2567 (or PORT environment variable.)
+listen(app);
